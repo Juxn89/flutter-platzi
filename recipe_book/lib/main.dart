4d27c3ed6a7e00d54783c2093f7b7a176898daea
Juan Gómez <gb.jc@outlook.com>
2025-10-20 22:12:20 +0000

    Added home screen page.

--- recipe_book/lib/main.dart
+++ recipe_book/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:recipe_book/screens/home_screen.dart';
 
 void main() {
   runApp(const MainApp());
@@ -21,39 +22,22 @@ class RecipeBook extends StatelessWidget {
 
 	@override
 	Widget build(BuildContext context){ 
-		return Scaffold(
+		return DefaultTabController(
+			length: 4, 
+			child: Scaffold(
 			appBar: AppBar(
 				backgroundColor: Colors.teal,
-				title: const Text('Recipe Book', style: TextStyle(color: Colors.white))
+				title: const Text('Recipe Book', style: TextStyle(color: Colors.white)),
+				bottom: TabBar(
+					indicatorColor: Colors.white,
+					labelColor: Colors.white,
+					tabs: [
+						Tab( icon: Icon(Icons.home), text: 'Home',)
+				]),
 			),
-			body: Container(
-				width: MediaQuery.of(context).size.width,
-				height: 125,
-				child: Card(
-					child: Row(
-						children: <Widget>[
-							Container(
-								height: 125,
-								width: 100,
-								child: ClipRRect(
-									borderRadius: BorderRadius.circular(10),
-									child: Container(),
-								)
-							),
-							SizedBox(width: 26,),
-							Column(children: <Widget>[
-								Text('Lassagna'),
-								Text('Juan Gómez'),
-								Container(
-									height: 2,
-									width: 75,
-									color: Colors.lightBlueAccent,
-								)
-							],)
-						],
-					),
-				),
-			),
-		);
+			body: TabBarView(children: [
+				HomeScreen()
+			])
+		));
 	}
 }
